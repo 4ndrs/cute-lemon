d5492994c75e0ac0b6e83ef60b39e26fc068b736
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-02-24 15:23:50 +0100

    Add the logo to Header and move the nav element to its own component

---
+++ src/components/Header/Nav.tsx
@@ -0,0 +1,35 @@
+import BasketIcon from "../../assets/basket.svg";
+
+const Nav = () => {
+  return (
+    <nav>
+      <menu>
+        <li>
+          <a href="#TBD">HOME</a>
+        </li>
+        <li>
+          <a href="#TBD">ABOUT</a>
+        </li>
+        <li>
+          <a href="#TBD">MENU</a>
+        </li>
+        <li>
+          <a href="#TBD">RESERVATIONS</a>
+        </li>
+        <li>
+          <a href="#TBD">ORDER ONLINE</a>
+        </li>
+        <li>
+          <a href="#TBD">LOGIN</a>
+        </li>
+        <li>
+          <a href="#TBD">
+            <img src={BasketIcon} alt="Basket icon" />
+          </a>
+        </li>
+      </menu>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
+import Nav from "./Nav";
+import Logo from "../../assets/logo.svg";
+
 const Header = () => {
   return (
     <header>
-      <nav>
-        <menu></menu>
-      </nav>
+      <img src={Logo} alt="Little Lemon logo" />
+      <Nav />
     </header>
   );
 };

6bb2dd46b82a656814a46fe07c7972bfff4d65ec
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-02-26 08:06:46 +0100

    Add comments service

---
+++ src/services/comments.ts
@@ -0,0 +1,60 @@
+// simulate some kind of api fetcher
+import { sleep } from "../utils";
+
+import JenniferPicture from "../assets/jennifer.jpg";
+import JohnPicture from "../assets/john.jpg";
+import BryantPicture from "../assets/bryant.jpg";
+import ElisaPicture from "../assets/elisa.jpg";
+
+import type { Comment } from "../types";
+
+const commentsData: Comment[] = [
+  {
+    id: 1,
+    name: "Jennifer Lopez",
+    nickname: "jennifer22",
+    imageSrc: JenniferPicture,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Praesent a urna nibh.",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "John Smith",
+    nickname: "smith33",
+    imageSrc: JohnPicture,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Praesent a urna nibh.",
+    rating: 4,
+  },
+  {
+    id: 3,
+    name: "Bryant Conner",
+    nickname: "bryant8",
+    imageSrc: BryantPicture,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Praesent a urna nibh.",
+    rating: 5,
+  },
+  {
+    id: 4,
+    name: "Elisa McGuire",
+    nickname: "eli77",
+    imageSrc: ElisaPicture,
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+      "Praesent a urna nibh.",
+    rating: 5,
+  },
+];
+
+const getComments = async () => {
+  await sleep(2);
+
+  return commentsData;
+};
+
+export { getComments };
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,12 @@ export type Dish = Readonly<{
   imageSrc: string;
   price: number;
 }>;
+
+export type Comment = Readonly<{
+  id: number;
+  name: string;
+  nickname: string;
+  imageSrc: string;
+  comment: string;
+  rating: number;
+}>;

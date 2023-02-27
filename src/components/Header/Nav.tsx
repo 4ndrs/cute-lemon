50debf222587def2f913ba32cd8d284e8b83660b
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-02-27 10:33:50 +0100

    Add initial Header.css & Nav.css

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,9 +1,11 @@
 import { mainMenu } from "../../links";
 
+import "./Nav.css";
+
 const Nav = () => {
   return (
     <nav aria-label="primary-navigation">
-      <menu>
+      <menu className="menu">
         {mainMenu.map((link) => (
           <li key={link.name}>
             <a href={link.href}>{link.name.toUpperCase()}</a>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,11 @@ import Nav from "./Nav";
 import Logo from "../../assets/logo.svg";
 import BasketIcon from "../../assets/basket.svg";
 
+import "./Header.css";
+
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <img src={Logo} alt="Little Lemon logo" />
       <Nav />
       <a href="#TBD">

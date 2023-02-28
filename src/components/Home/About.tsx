7f4afe1508312b1244f208c9a7a053288d2ffb61
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-02-28 08:48:06 +0100

    Add initial About.css

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -1,22 +1,28 @@
-import MarioAndAdrianB from "../../assets/mario_and_adrian_b.jpg";
+import MarioAndAdrianB from "../../assets/mario_and_adrian_b_crop.jpg";
 import Restaurant from "../../assets/restaurant.jpg";
 
-const About = () => (
-  <section>
-    <h1>Little Lemon</h1>
-    <h2>Chicago</h2>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in consequat
-      mi. Cras malesuada mi ligula, et rutrum metus dignissim ac. Fusce sed
-      semper leo, eget efficitur metus. Proin eu mi volutpat, lobortis ipsum sit
-      amet, dapibus elit. Proin sapien nisl, luctus non mauris semper, elementum
-      lobortis quam. Fusce a finibus nisl, id varius tortor. Suspendisse varius
-      lacus nec ultrices tincidunt. Quisque id ex tortor. Donec tempus neque
-      faucibus velit sagittis aliquam.
-    </p>
+import "./About.css";
 
-    <img src={MarioAndAdrianB} alt="Mario and Adrian" />
-    <img src={Restaurant} alt="Restaurant" />
+const About = () => (
+  <section className="about">
+    <div>
+      <h1>Little Lemon</h1>
+      <h2>Chicago</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in
+        consequat mi. Cras malesuada mi ligula, et rutrum metus dignissim ac.
+        Fusce sed semper leo, eget efficitur metus. Proin eu mi volutpat,
+        lobortis ipsum sit amet, dapibus elit. Proin sapien nisl, luctus non
+        mauris semper, elementum lobortis quam. Fusce a finibus nisl, id varius
+        tortor. Suspendisse varius lacus nec ultrices tincidunt. Quisque id ex
+        tortor. Donec tempus neque faucibus velit sagittis aliquam.
+      </p>
+    </div>
+
+    <div className="pictures">
+      <img src={Restaurant} alt="Restaurant" />
+      <img src={MarioAndAdrianB} alt="Mario and Adrian" />
+    </div>
   </section>
 );
 

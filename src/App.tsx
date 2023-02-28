40eaf9e4eab89ea358a547977696466c7001f1cd
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-02-28 19:01:51 +0100

    Add some initial media queries for responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header, Footer, Home } from "./components";
 
+import "./App.css";
+
 const App = () => {
   return (
-    <>
+    <div className="app-container">
       <Header />
       <Home />
       <Footer />
-    </>
+    </div>
   );
 };
 

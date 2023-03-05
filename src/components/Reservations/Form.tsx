856dd697cb6339cb77fdd2274af896bca30aa9f1
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-05 14:39:11 +0100

    Add a fieldset to the form
    An attempt to make it more semantically correct, since the legend tag
    seems more appropriate than a heading one for the details section

--- src/components/Reservations/Form.tsx
+++ src/components/Reservations/Form.tsx
@@ -56,90 +56,92 @@ const Form = () => {
         </label>
       </div>
 
-      <h2 className="lead-text">Details</h2>
+      <fieldset>
+        <legend className="lead-text">Details</legend>
 
-      <div className="diners-field">
-        <label htmlFor="diners">Diners</label>
-        <select
-          id="diners"
-          className={errors.diners && "invalid-input"}
-          {...register("diners", { required })}
-        >
-          <option>2</option>
-          <option>4</option>
-          <option>6</option>
-        </select>
-        <label htmlFor="diners" className="messages">
-          {errors.diners?.message}
-        </label>
-      </div>
+        <div className="diners-field">
+          <label htmlFor="diners">Diners</label>
+          <select
+            id="diners"
+            className={errors.diners && "invalid-input"}
+            {...register("diners", { required })}
+          >
+            <option>2</option>
+            <option>4</option>
+            <option>6</option>
+          </select>
+          <label htmlFor="diners" className="messages">
+            {errors.diners?.message}
+          </label>
+        </div>
 
-      <div className="occasion-field">
-        <label htmlFor="occasion">Occasion</label>
-        <select
-          id="occasion"
-          className={errors.occasion && "invalid-input"}
-          {...register("occasion", { required })}
-        >
-          <option>Standard</option>
-          <option>Birthday</option>
-          <option>Anniversary</option>
-          <option>Engagement</option>
-        </select>
-        <label htmlFor="occasion" className="messages">
-          {errors.occasion?.message}
-        </label>
-      </div>
+        <div className="occasion-field">
+          <label htmlFor="occasion">Occasion</label>
+          <select
+            id="occasion"
+            className={errors.occasion && "invalid-input"}
+            {...register("occasion", { required })}
+          >
+            <option>Standard</option>
+            <option>Birthday</option>
+            <option>Anniversary</option>
+            <option>Engagement</option>
+          </select>
+          <label htmlFor="occasion" className="messages">
+            {errors.occasion?.message}
+          </label>
+        </div>
 
-      <div className="area-field">
-        <label htmlFor="area">Area</label>
-        <select
-          id="area"
-          className={errors.area && "invalid-input"}
-          {...register("area", { required })}
-        >
-          <option>Storefront (Indoors)</option>
-          <option>Storefront (Outdoors)</option>
-          <option>Indoors (Zone A)</option>
-          <option>Indoors (Zone B)</option>
-        </select>
-        <label htmlFor="area" className="messages">
-          {errors.area?.message}
-        </label>
-      </div>
-      <div className="more-info">
-        <label htmlFor="info">Additional information</label>
-        <a href="#TBD" id="info">
-          <InfoIcon className="info-icon" />
-        </a>
-      </div>
+        <div className="area-field">
+          <label htmlFor="area">Area</label>
+          <select
+            id="area"
+            className={errors.area && "invalid-input"}
+            {...register("area", { required })}
+          >
+            <option>Storefront (Indoors)</option>
+            <option>Storefront (Outdoors)</option>
+            <option>Indoors (Zone A)</option>
+            <option>Indoors (Zone B)</option>
+          </select>
+          <label htmlFor="area" className="messages">
+            {errors.area?.message}
+          </label>
+        </div>
+        <div className="more-info">
+          <label htmlFor="info">Additional information</label>
+          <a href="#TBD" id="info">
+            <InfoIcon className="info-icon" />
+          </a>
+        </div>
 
-      <div className="text-fields">
-        <label htmlFor="name">Your name</label>
-        <input
-          type="text"
-          id="name"
-          className={errors.name && "invalid-input"}
-          {...register("name", { required })}
-        />
+        <div className="text-fields">
+          <label htmlFor="name">Your name</label>
+          <input
+            type="text"
+            id="name"
+            className={errors.name && "invalid-input"}
+            {...register("name", { required })}
+          />
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          className={errors.email && "invalid-input"}
-          {...register("email", { required })}
-        />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className={errors.email && "invalid-input"}
+            {...register("email", { required })}
+          />
 
-        <label htmlFor="comments">Comments (optional)</label>
-        <textarea
-          rows={5}
-          cols={33}
-          id="comments"
-          className={errors.comments && "invalid-input"}
-          {...register("comments")}
-        />
-      </div>
+          <label htmlFor="comments">Comments (optional)</label>
+          <textarea
+            rows={5}
+            cols={33}
+            id="comments"
+            className={errors.comments && "invalid-input"}
+            {...register("comments")}
+          />
+        </div>
+      </fieldset>
 
       <div className="form-navigation">
         <div className="page one"></div>

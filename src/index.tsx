8fce3f5e949889ce978dfc7d52c203ad4fd15f71
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-07 15:57:46 +0100

    Add router basename for github pages deployment

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import App from "./App";
 
 import "./index.css";
 
+const BASENAME = process.env.NODE_ENV === "production" ? "/cute-lemon" : "/";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASENAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>

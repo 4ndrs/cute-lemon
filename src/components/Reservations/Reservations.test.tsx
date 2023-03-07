d6201011a4f24c67f2a805bf7650402f8e5b8ec9
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-07 14:11:05 +0100

    Add test to verify it shows overview when submitting with form filled

--- src/components/Reservations/Reservations.test.tsx
+++ src/components/Reservations/Reservations.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { BookingProvider } from "../../context/booking";
 
 import Reservations from ".";
@@ -39,5 +39,53 @@ describe("Sanity tests", () => {
     userEvent.click(nextButton);
 
     await screen.findAllByText(/required/i);
+  });
+
+  test("Clicking the next button with the form filled shows overview", async () => {
+    render(
+      <ContextWrapper>
+        <Routes>
+          <Route path="/book/*" element={<Reservations />} />
+          <Route path="/" element={<Reservations />} />
+        </Routes>
+      </ContextWrapper>
+    );
+
+    const dateField = screen.getByLabelText("Date");
+    //const timeField = screen.getByLabelText("Time"); // this one is random
+    const dinersField = screen.getByLabelText("Diners");
+    const occasionField = screen.getByLabelText("Occasion");
+    const areaField = screen.getByLabelText("Area");
+    const nameField = screen.getByLabelText("Your name");
+    const emailField = screen.getByLabelText("Email");
+    const commentsField = screen.getByLabelText("Comments (optional)");
+    const nextButton = screen.getByRole("button", { name: "Next" });
+
+    await waitFor(() => expect(nextButton).not.toHaveAttribute("disabled"));
+    userEvent.type(dateField, "2023-02-28");
+
+    await waitFor(() => expect(nextButton).not.toHaveAttribute("disabled"));
+
+    userEvent.selectOptions(dinersField, "4");
+    userEvent.selectOptions(occasionField, "Anniversary");
+    userEvent.selectOptions(areaField, "Indoors (Zone B)");
+
+    await waitFor(() => expect(nextButton).not.toHaveAttribute("disabled"));
+
+    userEvent.type(nameField, "Liselotte Riefenstahl");
+    userEvent.type(emailField, "lise@tsunderais.co.jp");
+    userEvent.clear(commentsField);
+    userEvent.type(commentsField, "Testing Library rocks");
+
+    userEvent.click(nextButton);
+
+    await waitFor(() => {
+      screen.getByRole("button", { name: "Reserve" });
+    });
+
+    screen.getByText("Indoors (Zone B)");
+    screen.getByText("Liselotte Riefenstahl");
+    screen.getByText("Anniversary");
+    screen.getByText("Testing Library rocks");
   });
 });

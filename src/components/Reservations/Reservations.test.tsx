a78292f4c230153901d742843a74b7aaefdaaa1a
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-07 12:19:07 +0100

    Add test to verify it stays on the page when submitting without filling

--- src/components/Reservations/Reservations.test.tsx
+++ src/components/Reservations/Reservations.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { BrowserRouter } from "react-router-dom";
 import { BookingProvider } from "../../context/booking";
@@ -22,5 +23,21 @@ describe("Sanity tests", () => {
     const heading = screen.getByRole("heading").textContent;
 
     expect(heading).toBe("RESERVE A TABLE");
+  });
+
+  test("Clicking the next button without filling the form stays on page", async () => {
+    render(
+      <ContextWrapper>
+        <Reservations />
+      </ContextWrapper>
+    );
+
+    const nextButton = screen.getByRole("button", { name: "Next" });
+
+    await waitFor(() => expect(nextButton).not.toHaveAttribute("disabled"));
+
+    userEvent.click(nextButton);
+
+    await screen.findAllByText(/required/i);
   });
 });
